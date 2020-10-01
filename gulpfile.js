1f638f6aa1c11cb48af3012cb20869b8a6564ba6
Ryan Gittings <ryan@madebykingfisher.co.uk>
2020-10-01 14:14:26 +0100

    Remove JS transpiling in favour of modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,67 @@
 // Grab our gulp packages
 const $ = require('gulp-load-plugins')({
   pattern: ['*', '*/*'],
-  scope: ['devDependencies']
+  scope: ['devDependencies'],
 });
 
 const browserSync = $.browserSync.create();
 
 const paths = {
-  url: "blyth.test",
+  url: 'blyth.test',
   styles: {
-    watch: ["./assets/src/css/**/*.css"],
-    src: ["./assets/src/css/*.css"],
-    dest: "./assets/css"
+    watch: ['./assets/src/css/**/*.css'],
+    src: ['./assets/src/css/*.css'],
+    dest: './assets/css',
   },
   scripts: {
-    watch: ["./assets/src/js/**/*.js"],
+    watch: ['./assets/src/js/**/*.js'],
     src: './assets/src/js',
-    dest: "assets/js"
+    dest: 'assets/js',
   },
   assets: {
     src: ['./assets/src/img/**/*.+(png|jpg|jpeg|gif|svg)'],
-    dest: "./assets/img"
-  }
+    dest: './assets/img',
+  },
 };
-
-// Minify Javascript files
-$.gulp.task('scripts', function () {
-  var b = $.browserify({
-    entries: `${paths.scripts.src}/main.js`,
-    debug: true,
-  }).transform("babelify", {presets: ["@babel/preset-env"]});
-
-  return b.bundle()
-    .pipe($.plumber())
-    .pipe($.vinylSourceStream('main.js'))
-    .pipe($.vinylBuffer())
-    .pipe($.uglify())
-    .pipe($.sourcemaps.init({loadMaps: true}))
-    .pipe($.sourcemaps.write('./'))
-    .pipe($.gulp.dest(paths.scripts.dest));
-});
 
 // Compiles sass into Assets
 $.gulp.task('css', function () {
   return $.gulp
     .src(paths.styles.src)
-    .pipe($.postcss([$.postcssImport(), $.cssnano({
-      preset: ['default', {
-        calc: false,
-      }],
-    })]))
-    .pipe($.rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      $.postcss([
+        $.postcssImport(),
+        $.cssnano({
+          preset: [
+            'default',
+            {
+              calc: false,
+            },
+          ],
+        }),
+      ])
+    )
+    .pipe(
+      $.rename({
+        suffix: '.min',
+      })
+    )
     .pipe($.gulp.dest(paths.styles.dest))
     .pipe(browserSync.stream());
 });
 
 // Process and Optimizing Images
 $.gulp.task('assets', function () {
-  return $.gulp.src(paths.assets.src)
-    .pipe($.cache($.imagemin({
-      interlaced: true,
-    })))
-    .pipe($.gulp.dest(paths.assets.dest))
+  return $.gulp
+    .src(paths.assets.src)
+    .pipe(
+      $.cache(
+        $.imagemin({
+          interlaced: true,
+        })
+      )
+    )
+    .pipe($.gulp.dest(paths.assets.dest));
 });
 
 // Watchers
@@ -71,12 +69,12 @@ $.gulp.task('watch', function () {
   browserSync.init({
     proxy: paths.url,
     open: false,
-    notify: false
+    notify: false,
   });
 
   $.gulp.watch(paths.styles.watch, $.gulp.series('css'));
   $.gulp.watch(paths.scripts.watch, $.gulp.series('scripts'));
-  $.gulp.watch("**/*.php", reload);
+  $.gulp.watch('**/*.php', reload);
   $.gulp.watch('**/*.html', reload);
   $.gulp.watch(paths.assets.src, $.gulp.series('assets'));
 });
